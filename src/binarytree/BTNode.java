feea9bfe9c820b5ebd6eaa47bed897c8f386828a
Fred <114075370+Fred243C@users.noreply.github.com>
2024-02-29 11:00:15 +0000

    DSA First commit

---
+++ src/binarytree/BTNode.java
@@ -0,0 +1,58 @@
+/*
+ * BTNode.java
+ *
+ */
+package binarytree;
+
+public class BTNode{
+    private int element;
+    private BTNode leftChild;
+    private BTNode rightChild;
+
+    /** Creates a new instance of BTNode */
+    public BTNode(int theValue){
+        element = theValue;
+        leftChild = null;
+        rightChild = null;
+    }
+
+    public boolean isLeaf(){
+     	  return ((leftChild == null)&&(rightChild == null));
+     }
+
+    public int getElement (){
+        return element;
+    }
+
+    public void setLeft(BTNode theNode ){
+       leftChild = theNode;
+    }
+
+    public void setRight(BTNode theNode){
+       rightChild = theNode;
+    }
+
+    public BTNode getLeft( ){
+       return leftChild;
+    }
+
+    public BTNode getRight(){
+       return rightChild;
+    }
+
+    public boolean isInternal(){
+        if((leftChild != null) || (rightChild != null))
+            return true;
+        else
+            return false;
+    }
+
+    public boolean isExternal(){
+        if((leftChild == null) && (rightChild == null))
+            return true;
+        else
+            return false;
+    }
+
+}
+
